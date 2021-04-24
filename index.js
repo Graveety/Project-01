e50a3426b6a92cb5bc820d0833ff1590a5f0a89e
Graveety <oshomavictory@gmail.com>
2021-04-24 12:10:37 +0100

    added support for div

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
-function add(arg1,arg2){
-    return arg1+arg2
-}
 
+// Sum of two values
+function add(arg1,arg2) =>  arg1+arg2;
+
+// Substrate two values
+function substration(arg1, arg2) => arg1 - arg2
+
+// Divide two values
+function divide(arg1,arg2) => arg1/arg2;
+
+// display answer
 const result = add(2,2);
 
 console.log(result);
